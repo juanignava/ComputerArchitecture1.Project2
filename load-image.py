9bb2f71f84e4a81242834320cd6402d174b12e3b
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-04-30 20:51:01 -0600

    Añadir scripts de manejo de imagen

---
+++ load-image.py
@@ -0,0 +1,16 @@
+"""
+Script que corresponde al encargado de leer el txt
+generado en la simulación de Quartus y convertilo en
+una imagen
+"""
+
+# función que puede leer el archivo linea por linea
+
+
+# función que separa los valores contenidos en un número
+# de 32 bits en 4 valores de números de 8 bits que componen
+# la escala de grises
+
+
+# con la lista de la escala de grises generar la nueva imagen
+# con el zoom respectivo.
---
+++ save-image.py
@@ -0,0 +1,24 @@
+"""
+Scrit que toma la imagen a analizar y que le calcula
+la escala de grises a cada pixel y luego altera el
+archivo .mif de la memoria para añadirle la imagen
+a analizar
+"""
+
+# función de cambio de la imagen a escala de grises
+
+
+# función tomar 4 valores de escala de grises y convetirlos
+# en un número de 32 bits con los valores fusionados
+
+
+# función para alterar el archivo .mif en las direcciones
+# correspondientes de memoria
+
+
+# opcional:
+# este script también puede generar el .mif completo al
+# añadir las líneas respectivas de las instrucciones
+# para este caso se cuenta con la salida de las instrucciones
+# en binario del compilador y ahora se convierten en decimal
+# y por último se añaden en las primeras direcciones de memoria
