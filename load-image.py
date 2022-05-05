00c8d15e4cd7c216120bf8cf1e28255c2ed8f391
Juan Igancio Navarro <ju.navarro@estudiantec.cr>
2022-05-05 12:01:06 -0600

    Corrección de los scripts del proyecto
    Los scripts de entrada y salida creaban arhivos .mif pero como se cambio de desición para usar .txt entonces se hicieron los respectivos cambios.

--- load-image.py
+++ load-image.py
@@ -8,9 +8,9 @@ una imagen
 import numpy as np
 import imageio
 
-def leer_mif(ruta):
+def leer_mem_data_output(ruta):
     """
-    Esta función permite leer el archivo .mif para conseguir los
+    Esta función permite leer el archivo .txt para conseguir los
     datos guardados por el procesador al correr la simulación. Estos
     datos se guardarán en una lista que contiene la imagen con el
     zoom aplicado
@@ -20,58 +20,9 @@ def leer_mif(ruta):
     add_flag = False
     result_list = []
     for line in f:
-        if (line[0:6] == "\t33135"):
-            add_flag = True
-        if add_flag:
-            pixel = line[9:-2]
-            result_list.append(int(pixel))
-        if (line[0:6] == "\t65534"):
-            add_flag = False
-    
-    return result_list
+        if (line[0] != '/'):
+            result_list.append(int(line, 16))
 
-def completar_cero(bin):
-    """
-    Esta función completa el binario a un string de 32 elementos
-    completando con ceros
-    """
-    i = 0
-    result = ""
-    while (i < 32):
-        if (i < len(bin)):
-            result = result + bin[i]
-
-        else:
-            result = '0' + result
-        
-        i += 1
-
-    return result
-
-def separar_bits(lista_dec):
-    """
-    Esta función convierte la lista decimal a una lista más grande con
-    los 4 valores de 8 bits separados en otra lista para su posterior
-    análisis en imágenes.
-    """
-
-    # la lista será 4 veces más grande
-    i = 0
-    result_list = []
-    while (i < len(lista_dec)):
-        j = 0
-        elem = lista_dec[i]
-        elem = str(format(elem, "b"))
-        elem = completar_cero(elem)
-        for j in range(4):
-            if (j == 0):
-                elem_bin = elem[-8*(j+1):]
-            else:
-                elem_bin = elem[-8*(j+1):-8*j]
-            elem_dec = int(elem_bin, 2)
-            result_list.append(elem_dec)
-        i += 1
-    
     return result_list
 
 
@@ -94,18 +45,10 @@ def guardar_imagen(ruta, lista):
 
 
 
-# función que separa los valores contenidos en un número
-# de 32 bits en 4 valores de números de 8 bits que componen
-# la escala de grises
+# MAIN
+# se deben cargar los resultados en una lista para su analisis
+result_list = leer_mem_data_output("mem_data_output.txt")
 
 
-
-# con la lista de la escala de grises generar la nueva imagen
-# con el zoom respectivo.
-
-result_list = leer_mif("mem_data.mif")
-
-
-result_sep_list = separar_bits(result_list)
-
-guardar_imagen("images/test-image-grises.bmp", result_sep_list)
+# se guarda la imagen ya procesada
+guardar_imagen("images/test-image-grises.bmp", result_list)
